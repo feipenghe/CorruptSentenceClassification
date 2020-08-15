Fixed Average Perceptron Network

main.py
@@ -38,6 +38,7 @@ def train_one_epoch(model, train_loader, criterion, optimizer, padding_index):
     """
 
     # put model back into training mode
+    model.to(1)
     model.train()
 
     epoch_loss = 0.0
@@ -46,9 +47,9 @@ def train_one_epoch(model, train_loader, criterion, optimizer, padding_index):
         sentences, labels = batch
         optimizer.zero_grad()
 
-        logits = model(sentences, padding_index)
+        logits = model(sentences.to(1), padding_index)
         labels = labels.squeeze()
-        loss = criterion(logits, labels)
+        loss = criterion(logits, labels.to(1))
 
         # TODO: implement L2 loss here
         # Note: model.parameters() returns an iterator over the parameters
@@ -67,8 +68,10 @@ def train_one_epoch(model, train_loader, criterion, optimizer, padding_index):
         else:
             L2 = 0
 
-        regularized_loss = loss + 1e-4*L2
-
+        regularized_loss = loss + L2
+        import math
+        if math.isnan(loss):
+            exit()
         # backprop regularized loss and update parameters
         regularized_loss.backward()
         optimizer.step()
@@ -95,10 +98,10 @@ def evaluate(model, val_loader, criterion, padding_index):
     epoch_loss = 0.0
     for batch in val_loader:
         sentences, labels = batch
-        logits = model(sentences, padding_index)
+        logits = model(sentences.to(1), padding_index)
 
         labels = labels.squeeze()
-        loss = criterion(logits, labels)
+        loss = criterion(logits, labels.to(1))
         epoch_loss += loss.item()
     return epoch_loss / len(val_loader)
 
@@ -131,7 +134,7 @@ def main(args):
     set_seed(args.seed)
 
     # get iterators for data
-    train_data = SST2Dataset("./challenge-data/train.tsv", token_level="word", unk_cutoff=3)
+    train_data = SST2Dataset("./challenge-data/train_20000.tsv", token_level="word", unk_cutoff=3)
 
 
     # sampling
@@ -152,7 +155,7 @@ def main(args):
 
     # build model
     model = DeepAveragingNetwork(
-        len(train_data.vocab), args.embedding_dim, args.hidden_dim, 2)
+        train_data.vocab, args.embedding_dim, args.hidden_dim, 2)
 
     # set up optimizer
     optim = torch.optim.Adam(model.parameters(), lr=1e-3)
@@ -204,7 +207,7 @@ if __name__ == '__main__':
     # training arguments
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--seed', type=int, default=572)
-    parser.add_argument('--num_epochs', type=int, default=15)
+    parser.add_argument('--num_epochs', type=int, default=25)
     parser.add_argument('--patience', type=int, default=None)
     parser.add_argument('--L2', action="store_true")
     # data arguments

model.py
@@ -1,47 +1,52 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-
-class LinearLayer(nn.Module):
-
-    def __init__(self, input_size, output_size):
-        super(LinearLayer, self).__init__()
-        # linear layer has two parameters: weights and biases
-        self.weight = nn.Parameter(torch.Tensor(input_size, output_size))
-        self.bias = nn.Parameter(torch.Tensor(output_size))
-        # initialize the parameters
-        nn.init.xavier_uniform_(self.weight)
-        nn.init.uniform_(self.bias)
-
-    def forward(self, inputs):
-        """Compute f(x) = xW + b.
-
-        Arguments:
-            inputs: a tensor of shape (batch_size, input_size)
-
-        Returns:
-            a tensor of shape (batch_size, output_size)
-        """
-        # TODO: implement the forward pass of a linear layer.
-        # You will want to use torch.matmul, as well as tensor addition
-        return torch.matmul(inputs, self.weight) + self.bias
-
+from util import load_embedding_matrix
+#
+# class LinearLayer(nn.Module):
+#
+#     def __init__(self, input_size, output_size):
+#         super(LinearLayer, self).__init__()
+#         # linear layer has two parameters: weights and biases
+#         self.weight = nn.Parameter(torch.Tensor(input_size, output_size))
+#         self.bias = nn.Parameter(torch.Tensor(output_size))
+#         # initialize the parameters
+#         nn.init.xavier_uniform_(self.weight)
+#         nn.init.uniform_(self.bias)
+#
+#     def forward(self, inputs):
+#         """Compute f(x) = xW + b.
+#
+#         Arguments:
+#             inputs: a tensor of shape (batch_size, input_size)
+#
+#         Returns:
+#             a tensor of shape (batch_size, output_size)
+#         """
+#         # TODO: implement the forward pass of a linear layer.
+#         # You will want to use torch.matmul, as well as tensor addition
+#         return torch.matmul(inputs, self.weight) + self.bias
 
 class DeepAveragingNetwork(nn.Module):
 
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, num_classes):
+    def __init__(self, vocab, embedding_dim, hidden_dim, num_classes):
         super(DeepAveragingNetwork, self).__init__()
+
+        vocab_size = len(vocab)
 
         # embedding matrix
         self.embedding = nn.Embedding(
-            vocab_size, embedding_dim, padding_idx=1)
+            vocab_size, embedding_dim, max_norm = 10, padding_idx=0)
+        embedding_matrix =  load_embedding_matrix(vocab, use_glove= True)
+        self.embedding.weight.data = torch.nn.init.xavier_uniform_(embedding_matrix)
         # first hidden layer
-        self.hidden = LinearLayer(embedding_dim, hidden_dim)
-        # second hidden layer
-        self.hidden2 = LinearLayer(hidden_dim, hidden_dim)
-        # output layer
-        self.output = LinearLayer(hidden_dim, num_classes)
+        self.hidden = nn.Linear(embedding_dim, hidden_dim)
+        # # second hidden layer
+        # self.hidden2 = nn.Linear(hidden_dim, hidden_dim)
+        # # output layer
+        # self.hidden3 = nn.Linear(hidden_dim, hidden_dim)
+
+        self.output = nn.Linear(hidden_dim, num_classes)
 
     def forward(self, text, padding_index):
         # (batch_size, seq_len, embed_dim)
@@ -59,7 +64,9 @@ class DeepAveragingNetwork(nn.Module):
         bagged = torch.sum(embeddings, dim=1) / lengths
         # (batch_size, hidden_dim)
         hidden = F.relu(self.hidden(bagged))
-        hidden = F.relu(self.hidden2(hidden))
+        # hidden = F.relu(self.hidden2(hidden))
+        # hidden = F.relu(self.hidden3(hidden))
         # (batch_size, num_classes)
         logits = self.output(hidden)
+
         return logits